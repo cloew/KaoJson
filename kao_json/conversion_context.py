97ba151d1d37c8feb7095a09fb679164ca79f94b
Chris Loew <cloew123@gmail.com>
2015-10-22 16:06:52 -0600

    Added the basic Conversion Context

---
+++ kao_json/conversion_context.py
@@ -0,0 +1,12 @@
+from kao_decorators import proxy_for
+
+@proxy_for('config', ['newConverter'])
+class ConversionContext:
+    """ Represents the Configuration and Keyword Arguments provided to a Converter """
+    
+    def __init__(self, config, kwargs):
+        """ Initialize with the config and keyword arguments """
+        self.config = config
+        self.kwargs = kwargs
+        
+    
