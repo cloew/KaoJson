49368f975a00cf41d93746f21bfb1493f2ca7c4f
Chris Loew <cloew123@gmail.com>
2015-10-29 12:50:45 -0600

    Added providers to the installed packages

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from distutils.core import setup
 
 setup(name='kao_json',
-      version='0.2.0',
+      version='0.2.1',
       description='Python JSON Conversion Library',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['kao_json',
                 'kao_json.builders',
-                'kao_json.config'],
+                'kao_json.config',
+                'kao_json.providers'],
       install_requires = ['kao_decorators',
                           'kao_dict']
      )
