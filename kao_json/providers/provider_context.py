0bbda9d5a18fc72d3a47720d816e42d503822d74
Chris Loew <cloew123@gmail.com>
2015-10-29 13:05:56 -0600

    Renamed the name variable to attr

--- kao_json/providers/provider_context.py
+++ kao_json/providers/provider_context.py
@@ -3,8 +3,8 @@ from kao_dict import KaoDict
 class ProviderContext:
     """ Represents the context for converting using a provider """
     
-    def __init__(self, name, obj, args):
+    def __init__(self, attr, obj, args):
         """ Initialize with the attr name, object, and args """
-        self.name = name
+        self.attr = attr
         self.obj = obj
         self.args = KaoDict(args)
--- kao_json/providers/via_attr.py
+++ kao_json/providers/via_attr.py
@@ -2,14 +2,14 @@
 class ViaAttr:
     """ Provides a corresponding attr for the Provider Context """
     
-    def __init__(self, name=None):
+    def __init__(self, attr=None):
         """ Initialize with the attr name """
-        self.name = name
+        self.attr = attr
         
     def __call__(self, context):
         """ Return the value for the context """
-        return getattr(context.obj, self.getName(context))
+        return getattr(context.obj, self.getAttrName(context))
         
-    def getName(self, context):
-        """ Return the name """
-        return self.name if self.name is not None else context.name
+    def getAttrName(self, context):
+        """ Return the attr name """
+        return self.attr if self.attr is not None else context.attr
