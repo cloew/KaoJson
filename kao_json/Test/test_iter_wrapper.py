22140ac7b388a30da155013e12e0a034494fdd15
Chris Loew <cloew123@gmail.com>
2015-10-28 21:37:39 -0600

    Fixed the Iter Wrapper test

--- kao_json/Test/test_iter_wrapper.py
+++ kao_json/Test/test_iter_wrapper.py
@@ -1,4 +1,4 @@
-from ..iter_value import IterValue
+from ..iter_wrapper import IterWrapper
 import unittest
 
 class IterValueTest(unittest.TestCase):
@@ -7,14 +7,14 @@ class IterValueTest(unittest.TestCase):
     def test_iterable(self):
         """ Test that an iterable is returned properly """
         expected = [1,2,3]
-        actual = IterValue(expected)
+        actual = IterWrapper(expected)
         
         self.assertEqual(expected, actual)
         
     def test_nonIterable(self):
         """ Test that a non-iterable is returned properly """
         expected = 1
-        actual = IterValue(expected)
+        actual = IterWrapper(expected)
         
         self.assertEqual(len(actual), 1)
         self.assertIn(expected, actual)
