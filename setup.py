ab087885268819b46413ce41b3ec6b2aee8bf598
Chris Loew <cloew123@gmail.com>
2016-05-07 10:00:39 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_json',
-      version='0.2.2',
+      version='0.3.0',
       description='Python JSON Conversion Library',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
